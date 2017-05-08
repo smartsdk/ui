a0b4583210b70240af30f870daeb751617b22cd8
Gabriele Cerfoglio <gabriele@Gabrieles-MacBook-Pro.local>
2017-05-08 16:49:12 +0200

    Fixes for fiware UI

--- app/admin-tab/auth/fiware/controller.js
+++ app/admin-tab/auth/fiware/controller.js
@@ -153,7 +153,35 @@ export default Ember.Controller.extend({
         'accessMode': 'restricted',
         'allowedIdentities': [],
       });
-      model.save();
+
+      let url = window.location.href;
+
+      model.save().then(() => {
+        // Set this to true so the token will be sent with the request
+        this.set('access.enabled', true);
+
+        return this.get('userStore').find('setting', denormalizeName(C.SETTING.API_HOST)).then((setting) => {
+          if ( setting.get('value') )
+          {
+            this.send('waitAndRefresh', url);
+          }
+          else
+          {
+            // Default the api.host so the user won't have to set it in most cases
+            if ( window.location.hostname === 'localhost' ) {
+              this.send('waitAndRefresh', url);
+            } else {
+              setting.set('value', window.location.origin);
+              return setting.save().then(() => {
+                this.send('waitAndRefresh', url);
+              });
+            }
+          }
+        });
+      }).catch((err) => {
+        this.set('access.enabled', false);
+        this.send('gotError', err);
+      });
       return;
 
       /*
