5ae602fa23de8cdba3fa23d5ff24f4c6ff5340c6
Gabriele Cerfoglio <gabriele@Gabrieles-MacBook-Pro.local>
2017-05-09 15:49:00 +0200

    Fixes for fiware UI

--- app/application/route.js
+++ app/application/route.js
@@ -156,7 +156,7 @@ export default Ember.Route.extend({
       this.controllerFor('application').set('isPopup', true);
     }
 
-    if ( params.isTest ) {
+    if ( window.opener ) {
       console.log("is test");
       if ( fiware.stateMatches(params.state) ) {
         replyFiware(params.error_description, params.code);
