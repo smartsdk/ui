6f6d632f78d05adeb46dd377704ddd2bcc13887c
Gabriele Cerfoglio <gabriele@Gabrieles-MacBook-Pro.local>
2017-05-15 17:54:34 +0200

    fix

--- app/admin-tab/auth/controller.js
+++ app/admin-tab/auth/controller.js
@@ -10,6 +10,7 @@ export default Ember.Controller.extend({
       {route: 'admin-tab.auth.activedirectory', label: 'Active Directory',  css: 'activedirectory', available: this.hasRecord('ldapconfig')  },
       {route: 'admin-tab.auth.azuread',         label: 'Azure AD',          css: 'azuread',         available: this.hasRecord('azureadconfig')  },
       {route: 'admin-tab.auth.github',          label: 'GitHub',            css: 'github',          available: this.hasRecord('githubconfig')  },
+      {route: 'admin-tab.auth.fiware',          label: 'Fiware',            css: 'fiware',          available: this.hasRecord('fiwareconfig')  },
       {route: 'admin-tab.auth.localauth',       label: 'Local',             css: 'local',           available: this.hasRecord('localauthconfig')  },
       {route: 'admin-tab.auth.openldap',        label: 'OpenLDAP',          css: 'openldap',        available: this.hasRecord('openldapconfig')  },
       {route: 'admin-tab.auth.shibboleth',      label: 'Shibboleth',        css: 'shibboleth',      available: this.hasRecord('shibbolethconfig')  },
